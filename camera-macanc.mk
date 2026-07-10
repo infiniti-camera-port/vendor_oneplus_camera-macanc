7bef7b313ce1660e13ecc3d18725590358b7def4
1vivy <1vivy@tutanota.com>
2026-07-10 04:30:41 +0000

    camera-macanc: report stock OTA version
    Change-Id: I17c57ea4fb4d3cfc4babbfc2780059b08de5a4a7

--- camera-macanc.mk
+++ camera-macanc.mk
@@ -6,3 +6,6 @@
 $(call inherit-product, vendor/oneplus/camera-sm8850-common/camera-sm8850-common.mk)
 
 $(call inherit-product, proprietary/vendor/oneplus/camera-macanc/camera-macanc-vendor.mk)
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.build.version.ota=PLR110_11.A.62_0620_202606152334
